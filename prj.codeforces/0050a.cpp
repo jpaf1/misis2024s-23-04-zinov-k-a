c010e72d1b0a3173383e95374efca9bdf28f28c8
jpaf1 <145321333+jpaf1@users.noreply.github.com>
2024-03-17 12:17:41 +0300

    Create 0050a.cpp

---
+++ prj.codeforces/0050a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+
+int main(){
+   double m, n;
+   std::cin >> m >> n;
+   std::cout << std::floor(n * m * 0.5) << std::endl;
+}
